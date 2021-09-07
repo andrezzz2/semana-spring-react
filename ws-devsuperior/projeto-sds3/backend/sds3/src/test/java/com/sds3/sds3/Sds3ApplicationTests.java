b011a65e740f8676bdc2853a1d23c86cfe4b983b
André <alfr2@cin.ufpe.br>
2021-09-06 21:53:23 -0300

    project created

---
+++ ws-devsuperior/projeto-sds3/backend/sds3/src/test/java/com/sds3/sds3/Sds3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds3.sds3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sds3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
